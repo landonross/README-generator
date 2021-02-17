e7f53b219e69d69d0f6b62f1e9bc57eee1a10b46
Landon Ross <landon.w.ross@gmail.com>
2021-02-16 19:55:42 -0700

    Multiple updates to js files as well as installed the node_module

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# README-generator
-A node.js prompt file that will help to effectively and efficiently build a README file for your projects.
+
+# README generator
+
+A terminal/gitbash prompter that helps the user efficiently make a readme file for their projects and in-so-doing, save time and be more efficient
+
+* [Installation](#Installation)
+
+* [Instructions](#Instructions)
+
+* [License](#License)
+
+* [Contributors](#Contributors)
+
+* [Author](#Author)
+
+* [Tests](#Tests)
+
+## Installation
+By clicking that download
+
+## Instructions
+Once you have the code in Visual Studios, pull up the terminal and run "node index.js" and follow the prompts
+```
+
+## License 
+This project is licensed under the Blue - see the file for details
+
+## Contributors
+Landon Ross
+
+## Tests
+not sure what this questions entails
+
+## Questions
+
+Email: landon.w.ross@gmail.com
+
+GitHub: https://github.com/landonross
---
+++ index.js
@@ -0,0 +1,100 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const promptUser = () =>
+  inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Desired title:',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Give a description:',
+      },
+      {
+        type: 'input',
+        name: 'instructions',
+        message: 'How to install:',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Instructions for application:',
+      },
+      {
+        type: 'input',
+        name: 'contributers',
+        message: 'contributers and their responsibilities:',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for application:',
+        choices: [
+            'Red',
+            'Green',
+            'Blue'
+          ],
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github username?',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'Examples of how to run tests:',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+      }
+  ]);
+
+
+
+
+  const generateREADME = (answers) =>
+  `
+# ${answers.title}
+
+${answers.description}
+\n* [Installation](#Installation)
+\n* [Instructions](#Instructions)
+\n* [License](#License)
+\n* [Contributors](#Contributors)
+\n* [Author](#Author)
+\n* [Tests](#Tests)
+
+## Installation
+${answers.instructions}
+
+## Instructions
+${answers.usage}
+\`\`\`
+
+## License 
+This project is licensed under the ${answers.license} - see the file for details
+
+## Contributors
+${answers.contributers}
+
+## Tests
+${answers.test}
+
+## Questions
+\nEmail: ${answers.email}
+\nGitHub: https://github.com/${answers.github}
+`;
+
+promptUser()
+  .then((answers) => writeFileAsync('README.md', generateREADME(answers)))
+  .then(() => console.log('Success! Your README.md file has been generated'))
+  .catch((err) => console.error(err));
